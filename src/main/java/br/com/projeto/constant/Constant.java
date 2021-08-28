4709ae28e96473949906e93ad47bca31b6f03319
unknown <aannddrree>
2021-08-28 09:49:24 -0300

    Teste Constant

--- src/main/java/br/com/projeto/constant/Constant.java
+++ src/main/java/br/com/projeto/constant/Constant.java
@@ -5,4 +5,6 @@ public class Constant {
     public static final String API_URL = "projeto/api/v1/";
 
     public static final String API_CLIENT = API_URL + "client";
+
+    public static final String TEST = "test";
 }
